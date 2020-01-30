1ac4c3a65355d142f641a36867e12fccff11ff8b
imsuarezprieto <imsuarezprieto@gmail.com>
2020-01-30 10:26:55 +0100

    Añadir controlador para Película por plantilla.

---
+++ Filmian/Controllers/PeliculasController.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Filmian.Models;
+
+namespace Filmian.Controllers
+{
+    public class PeliculasController : Controller
+    {
+        private readonly DBContext _context;
+
+        public PeliculasController(DBContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Peliculas
+        public async Task<IActionResult> Index()
+        {
+            var dBContext = _context.Peliculas.Include(p => p.Director);
+            return View(await dBContext.ToListAsync());
+        }
+
+        // GET: Peliculas/Details/5
+        public async Task<IActionResult> Details(short? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var pelicula = await _context.Peliculas
+                .Include(p => p.Director)
+                .FirstOrDefaultAsync(m => m.PeliculaId == id);
+            if (pelicula == null)
+            {
+                return NotFound();
+            }
+
+            return View(pelicula);
+        }
+
+        // GET: Peliculas/Create
+        public IActionResult Create()
+        {
+            ViewData["DirectorId"] = new SelectList(_context.Directors, "DirectorID", "DirectorID");
+            return View();
+        }
+
+        // POST: Peliculas/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("PeliculaId,Titulo,Duracion,DirectorId")] Pelicula pelicula)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(pelicula);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["DirectorId"] = new SelectList(_context.Directors, "DirectorID", "DirectorID", pelicula.DirectorId);
+            return View(pelicula);
+        }
+
+        // GET: Peliculas/Edit/5
+        public async Task<IActionResult> Edit(short? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var pelicula = await _context.Peliculas.FindAsync(id);
+            if (pelicula == null)
+            {
+                return NotFound();
+            }
+            ViewData["DirectorId"] = new SelectList(_context.Directors, "DirectorID", "DirectorID", pelicula.DirectorId);
+            return View(pelicula);
+        }
+
+        // POST: Peliculas/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(short id, [Bind("PeliculaId,Titulo,Duracion,DirectorId")] Pelicula pelicula)
+        {
+            if (id != pelicula.PeliculaId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(pelicula);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!PeliculaExists(pelicula.PeliculaId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["DirectorId"] = new SelectList(_context.Directors, "DirectorID", "DirectorID", pelicula.DirectorId);
+            return View(pelicula);
+        }
+
+        // GET: Peliculas/Delete/5
+        public async Task<IActionResult> Delete(short? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var pelicula = await _context.Peliculas
+                .Include(p => p.Director)
+                .FirstOrDefaultAsync(m => m.PeliculaId == id);
+            if (pelicula == null)
+            {
+                return NotFound();
+            }
+
+            return View(pelicula);
+        }
+
+        // POST: Peliculas/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(short id)
+        {
+            var pelicula = await _context.Peliculas.FindAsync(id);
+            _context.Peliculas.Remove(pelicula);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool PeliculaExists(short id)
+        {
+            return _context.Peliculas.Any(e => e.PeliculaId == id);
+        }
+    }
+}
